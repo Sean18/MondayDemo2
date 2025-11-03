88edaa6272594415367863e8478a9c827020e0e2
Sean McKenna <seanmckenna18@gmail.com>
2025-11-03 14:35:12 -0500

    My edits from GH Desktop

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const name = url.searchParams.get('name') || 'World';
+  res.end(`Hello, ${name}\n`);
 });
 
 server.listen(port, hostname, () => {
